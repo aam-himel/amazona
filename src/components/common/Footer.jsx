84af551e25de5d6ef2131e83cfc7defa8bc46ff0
aamhimel <aam.himel@gmail.com>
2021-10-21 03:49:43 +0600

    footer edited

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,42 +1,23 @@
-import * as Route from 'constants/routes';
-import logo from 'images/logo-full.png';
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { footerLinks } from "constants/footer-links";
+import * as Route from "constants/routes";
+import logo from "images/logo-full.png";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
   const { pathname } = useLocation();
+  const d = new Date();
+  const currentYear = d.getFullYear();
 
-  const visibleOnlyPath = [
-    Route.HOME,
-    Route.SHOP
-  ];
+  // var d = new Date(); page.currentYear = d.getFullYear();
+  const visibleOnlyPath = [Route.HOME, Route.SHOP];
 
   return !visibleOnlyPath.includes(pathname) ? null : (
     <footer className="footer ">
-      <div className="footer-col-1">
-        <strong>
-          <span>
-            Developed by
-            {' '}
-            <a href="https://github.com/FariyaIslam">FARIYA ISLAM</a>
-          </span>
-        </strong>
+      <div className="footer-center">
+        <h3>AMAZONA </h3>
+        <small>&copy; Copyright {currentYear}, Amazona</small>
       </div>
-      {/* <div className="footer-col-2">
-        <img alt="Footer logo" className="footer-logo" src={logo} />
-        <h5>
-          &copy;&nbsp;
-          {new Date().getFullYear()}
-        </h5>
-      </div> */}
-      {/* <div className="footer-col-3">
-        <strong>
-          <span>
-            Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
-          </span>
-        </strong>
-      </div> */}
     </footer>
   );
 };
---
+++ src/constants/footer-links.js
@@ -0,0 +1,20 @@
+const footerLinks = [
+  {
+    path: "/",
+    text: "Home",
+  },
+  {
+    path: "/featured",
+    text: "Featured Product",
+  },
+  {
+    path: "/shop",
+    text: "Shop",
+  },
+  {
+    path: "/auto-mobile",
+    text: "Auto Mobile",
+  },
+];
+
+export { footerLinks };
